6f45f677500471809d0ed1a1a7f98b80a3dd24c4
Ali-Danish-72 <138890007+Ali-Danish-72@users.noreply.github.com>
2023-09-19 20:26:13 +0400

    Rush 00
    This is the only Rush project I attempted in the Piscine.

---
+++ Rush_00/ex00/rush00.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdanish <mdanish@student.42abudhabi.ae>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 11:59:55 by mdanish           #+#    #+#             */
+/*   Updated: 2023/05/06 18:06:52 by mdanish          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print(int no_of_cols, char left, char mid, char right)
+{
+	ft_putchar(left);
+	no_of_cols = no_of_cols - 1;
+	while (no_of_cols > 1)
+	{
+		ft_putchar(mid);
+		no_of_cols --;
+	}
+	if (no_of_cols == 1)
+	{
+		ft_putchar(right);
+		ft_putchar('\n');
+	}
+}
+
+void	rush(int x, int y)
+{
+	if (x >= 1 && y >= 1)
+	{
+		ft_print(x, 'o', '-', 'o');
+		y = y - 1;
+		while (y > 1)
+		{
+			ft_print(x, '|', ' ', '|');
+			y--;
+		}
+		if (y == 1)
+		{
+			ft_print(x, 'o', '-', 'o');
+		}
+	}
+	else
+		write(1, "You're stupid, just type an integer.\n", 37);
+}
